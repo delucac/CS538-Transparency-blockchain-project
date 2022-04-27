9b567ead0fd1b94f4f8b963cb6beea611ecef8a3
delucac <delucac@sunypoly.edu>
2022-04-26 23:11:23 -0400

    fixed some errors in file

--- actions.py
+++ actions.py
@@ -5,20 +5,21 @@ from algosdk import account, mnemonic, constants
 from algosdk.v2client import algod
 from algosdk.future import transaction
 
-private_key = 
+f = open("accounts.json")
+jsonDict = json.load(f)
+
+private_key = jsonDict["accounts"][1]['address']
 
 #connection parameters
 algod_address = "http://localhost:4001"
 algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 algod_client = algod.AlgodClient(algod_token, algod_address)
 
-
-
 def contract_optIn(private_key, my_address):
     #Make transaction
     params = algod_client.suggested_params()
     sender = account.address_from_private_key(private_key)
-    txn = ApplicationOptInTxn(sender, params, index)
+    txn = ApplicationOptInTxn(sender, params, 85862110)
     
     #sign
     signed_txn = unsigned_txn.sign(private_key)
@@ -37,11 +38,11 @@ def contract_optIn(private_key, my_address):
     print("Transaction information: {}".format(
         json.dumps(confirmed_txn, indent=4)))
         
-def contract_requestNotes(private_key, index):
+def contract_requestNotes(private_key):
     #Make transaction
     params = algod_client.suggested_params()
     sender = account.address_from_private_key(private_key)
-    txn = ApplicationNoOpTxn(sender, params, index, app_args)
+    txn = ApplicationNoOpTxn(sender, params, 85862110, "Request")
     
     #sign
     signed_txn = unsigned_txn.sign(private_key)
@@ -63,8 +64,73 @@ def contract_requestNotes(private_key, index):
 
 
 def contract_returnNotes():
+    #Make transaction
+    params = algod_client.suggested_params()
+    sender = account.address_from_private_key(private_key)
+    txn = ApplicationNoOpTxn(sender, params, 85862110, "Return")
+    
+    #sign
+    signed_txn = unsigned_txn.sign(private_key)
+    tx_id = signed_txn.transaction,get_txid()
+    
+    #send
+    client.send_transactions([signed_txn])
+    
+    # wait for confirmation
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)
+    except Exception as err:
+        print(err)
+        return
+        
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+        
     
 def contract_startMeeting():
+    #Make transaction
+    params = algod_client.suggested_params()
+    sender = account.address_from_private_key(private_key)
+    txn = ApplicationNoOpTxn(sender, params, 85862110, "Start")
+    
+    #sign
+    signed_txn = unsigned_txn.sign(private_key)
+    tx_id = signed_txn.transaction,get_txid()
+    
+    #send
+    client.send_transactions([signed_txn])
+    
+    # wait for confirmation
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)
+    except Exception as err:
+        print(err)
+        return
+        
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+        
     
 def contract_stopMeeting():
+    #Make transaction
+    params = algod_client.suggested_params()
+    sender = account.address_from_private_key(private_key)
+    txn = ApplicationNoOpTxn(sender, params, 85862110, "Stop")
     
+    #sign
+    signed_txn = unsigned_txn.sign(private_key)
+    tx_id = signed_txn.transaction,get_txid()
+    
+    #send
+    client.send_transactions([signed_txn])
+    
+    # wait for confirmation
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)
+    except Exception as err:
+        print(err)
+        return
+        
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+        
